c0e4931843a671ab67b587067002109db8de495b
SweetCinnamonBun <roman.klemiato18@gmail.com>
2023-12-04 14:56:25 +0200

    made the db connection

---
+++ Models/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestDb.Models;
+
+public partial class Ingredient
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
---
+++ Models/RecipesWebsiteDbTestContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestDb.Models;
+
+public partial class RecipesWebsiteDbTestContext : DbContext
+{
+    public RecipesWebsiteDbTestContext()
+    {
+    }
+
+    public RecipesWebsiteDbTestContext(DbContextOptions<RecipesWebsiteDbTestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Ingredient> Ingredients { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:recipesdbservertest.database.windows.net,1433;Initial Catalog=RecipesWebsiteDbTest;Persist Security Info=False;User ID=recipesdbservertest;Password=5544321A!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ingredient>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Ingredie__3213E83FD931FD8D");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
